f4c6d41dbc25d01b8379c99c253fb1bc13c2e46e
Vryon Antonio <vryonantonio@gmail.com>
2026-06-05 22:43:49 +0800

    darker shade of black bg

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SkillsSection from "./components/SkillsSection";
 
 function App() {
   return (
-    <div className="w-full min-h-screen bg-neutral-800">
+    <div className="w-full min-h-screen bg-neutral-900">
       <main className="mx-auto max-w-125 px-4 py-6 space-y-4">
         <ProfileCard />
         <CurrentProject />
